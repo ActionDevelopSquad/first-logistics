26b33a87121536ea4e451799125bb04f80fd221c
Lim JinKeon <0907john24@gmail.com>
2026-03-26 15:53:16 +0900

    chore: 초기 설정

---
+++ gw-service/src/test/java/com/firstlogistics/gwservice/GwServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstlogistics.gwservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
