ee33e81aee2535452687d0b9235b92d9e2e7c83e
haeun228 <123haeun@naver.com>
2026-03-27 19:17:46 +0900

    refactor: order 에러코드 컨벤션에 맞게 수정

--- order-service/src/main/java/com/firstlogistics/orderservice/domain/exception/OrderErrorCode.java
+++ order-service/src/main/java/com/firstlogistics/orderservice/domain/exception/OrderErrorCode.java
@@ -9,10 +9,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum OrderErrorCode implements ErrorCode {
 
-    ORDER_ITEM_NOT_EXIST(HttpStatus.BAD_REQUEST, "ORD_001", "주문 상품은 1개 이상이어야 합니다."),
-    INVALID_ORDER_STATUS(HttpStatus.BAD_REQUEST, "ORD_002", "허용되지 않은 주문 상태 변경입니다."),
-    DELIVERY_ALREADY_ASSIGNED(HttpStatus.BAD_REQUEST, "ORD_003", "이미 배송이 할당된 주문입니다."),
-    INVALID_ORDER_STATUS_FOR_DELIVERY(HttpStatus.BAD_REQUEST, "ORD_004", "배송은 승인된 주문에서만 가능합니다.");
+    ORDER_ITEM_NOT_EXIST(HttpStatus.BAD_REQUEST, "ORD001", "주문 상품은 1개 이상이어야 합니다."),
+    INVALID_ORDER_STATUS(HttpStatus.BAD_REQUEST, "ORD002", "허용되지 않은 주문 상태 변경입니다."),
+    DELIVERY_ALREADY_ASSIGNED(HttpStatus.BAD_REQUEST, "ORD003", "이미 배송이 할당된 주문입니다."),
+    INVALID_ORDER_STATUS_FOR_DELIVERY(HttpStatus.BAD_REQUEST, "ORD004", "배송은 승인된 주문에서만 가능합니다.");
 
     private final HttpStatus status;
     private final String code;
