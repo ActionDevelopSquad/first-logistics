91ca1727648710492b06ed30dac6ebc93f8f5d76
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2026-03-31 15:12:04 +0900

    fix: @RestController로 변경

--- notification-service/src/main/java/com/firstlogistics/notificationservice/ailog/presentation/AILogApiController.java
+++ notification-service/src/main/java/com/firstlogistics/notificationservice/ailog/presentation/AILogApiController.java
@@ -6,12 +6,12 @@ import com.firstlogistics.notificationservice.ailog.presentation.dto.response.AI
 import common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/ai-logs")
 public class AILogApiController {
