2b545691589be08af1c36d0cf8e048373bf7b46e
Sehi55 <sehi1028@naver.com>
2026-04-02 01:38:56 +0900

    fix: @Validated 어노테이션 누락으로 인한 요청 파라미터 검증 미작동 오류 수정

--- hub-service/src/main/java/com/firstlogistics/hubservice/hub/presentation/HubApiController.java
+++ hub-service/src/main/java/com/firstlogistics/hubservice/hub/presentation/HubApiController.java
@@ -18,6 +18,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -25,6 +26,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/hubs")
 @RequiredArgsConstructor
+@Validated
 public class HubApiController {
 
     private final HubCommandService hubCommandService;
