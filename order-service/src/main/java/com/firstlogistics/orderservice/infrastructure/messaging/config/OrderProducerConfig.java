68e4151c094bd9988085ad49ede2d2ecdef70164
haeun228 <123haeun@naver.com>
2026-04-01 16:34:08 +0900

    feat: OrderProducerConfig 추가

---
+++ order-service/src/main/java/com/firstlogistics/orderservice/infrastructure/messaging/config/OrderProducerConfig.java
@@ -0,0 +1,33 @@
+package com.firstlogistics.orderservice.infrastructure.messaging.config;
+
+import common.kafka.ConsistentHashPartitioner;
+import common.kafka.config.KafkaProducerConfig;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class OrderProducerConfig {
+
+    private final KafkaProducerConfig kafkaProducerConfig;
+
+    @Bean
+    public ProducerFactory<String, Object> orderProducerFactory() {
+        Map<String, Object> props = kafkaProducerConfig.commonProducerProps();
+        props.put(ProducerConfig.PARTITIONER_CLASS_CONFIG,
+                ConsistentHashPartitioner.class);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> orderKafkaTemplate() {
+        return new KafkaTemplate<>(orderProducerFactory());
+    }
+}
