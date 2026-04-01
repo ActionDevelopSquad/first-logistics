d3d1bef9d2e2e718855bf9827d26f587893e1532
Lim JinKeon <0907john24@gmail.com>
2026-04-01 23:02:18 +0900

    feat: feignClient 구현

--- user-service/src/main/java/com/firstlogistics/userservice/infrastructure/fegin/client/CompanyClient.java
+++ user-service/src/main/java/com/firstlogistics/userservice/infrastructure/fegin/client/CompanyClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.UUID;
 
-@FeignClient(name = "company-service")
+@FeignClient(name = "company-service", url = "http://localhost:8083")
 public interface CompanyClient {
 
     @GetMapping("/api/v1/companies/{companyId}")
--- user-service/src/main/java/com/firstlogistics/userservice/infrastructure/fegin/client/HubClient.java
+++ user-service/src/main/java/com/firstlogistics/userservice/infrastructure/fegin/client/HubClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.UUID;
 
-@FeignClient(name = "hub-service")
+@FeignClient(name = "hub-service", url = "http://localhost:8082")
 public interface HubClient {
 
     @GetMapping("/api/v1/hubs/{hubId}")
