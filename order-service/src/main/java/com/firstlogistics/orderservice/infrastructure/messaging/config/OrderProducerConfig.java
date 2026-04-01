21a65e2794cae8280a024af22a64f1bea82f086e
haeun228 <123haeun@naver.com>
2026-04-01 19:33:01 +0900

    test: Kafka 설정 격리 & 환경 의존성 제거

--- order-service/src/main/java/com/firstlogistics/orderservice/infrastructure/messaging/config/OrderProducerConfig.java
+++ order-service/src/main/java/com/firstlogistics/orderservice/infrastructure/messaging/config/OrderProducerConfig.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -14,7 +13,6 @@ import org.springframework.kafka.core.ProducerFactory;
 import java.util.Map;
 
 @Configuration
-@Profile("!test")
 @RequiredArgsConstructor
 public class OrderProducerConfig {
 
