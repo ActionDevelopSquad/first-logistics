1b50694e36a83dbdc05bd9edfc73c37ca3a433a1
Sehi55 <sehi1028@naver.com>
2026-04-01 16:24:36 +0900

    fix: 허브 생성 컨트롤러 메서드에 @valid 어노테이션 추가

--- hub-service/src/main/java/com/firstlogistics/hubservice/hubconnection/presentation/HubConnectionApiController.java
+++ hub-service/src/main/java/com/firstlogistics/hubservice/hubconnection/presentation/HubConnectionApiController.java
@@ -4,9 +4,11 @@ import com.firstlogistics.hubservice.hubconnection.application.HubConnectionComm
 import com.firstlogistics.hubservice.hubconnection.presentation.dto.request.CreateHubConnectionRequest;
 import com.firstlogistics.hubservice.hubconnection.presentation.dto.response.HubConnectionResponse;
 import common.response.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +20,7 @@ public class HubConnectionApiController {
     private final HubConnectionCommandService commandService;
 
     @PostMapping
-    public ResponseEntity<ApiResponse<HubConnectionResponse>> create(CreateHubConnectionRequest request){
+    public ResponseEntity<ApiResponse<HubConnectionResponse>> create(@Valid @RequestBody CreateHubConnectionRequest request){
         HubConnectionResponse response = HubConnectionResponse.from(commandService.create(request.toCommand()));
         return ResponseEntity.status(HubConnectionSuccessCode.HUB_CREATED.getStatus())
                 .body(ApiResponse.success(HubConnectionSuccessCode.HUB_CREATED,response));
