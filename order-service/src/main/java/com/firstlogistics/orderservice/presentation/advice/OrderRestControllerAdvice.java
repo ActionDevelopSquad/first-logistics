619b399960392d8ad96241c0492b086f071b02c2
haeun228 <123haeun@naver.com>
2026-04-01 23:59:48 +0900

    feat: OrderRestControllerAdvice

---
+++ order-service/src/main/java/com/firstlogistics/orderservice/presentation/advice/OrderRestControllerAdvice.java
@@ -0,0 +1,9 @@
+package com.firstlogistics.orderservice.presentation.advice;
+
+import common.exception.GlobalExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class OrderRestControllerAdvice extends GlobalExceptionHandler {
+}
+
