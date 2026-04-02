d13be34b7d1f23dcb478e9b9758c46d3e4691b68
jaebin1234 <jaebin2586@gmail.com>
2026-04-02 12:34:42 +0900

    fix: gethubs 파라미터 hubIds=id1,id2 형식으로 전달

--- delivery-service/src/main/java/com/firstlogistics/deliverservice/infrastructure/feign/HubClient.java
+++ delivery-service/src/main/java/com/firstlogistics/deliverservice/infrastructure/feign/HubClient.java
@@ -5,6 +5,7 @@ import com.firstlogistics.deliverservice.infrastructure.feign.config.FeignErrorD
 import com.firstlogistics.deliverservice.infrastructure.feign.dto.FeignResponse;
 import com.firstlogistics.deliverservice.application.port.dto.HubRouteResponse;
 import com.firstlogistics.deliverservice.application.port.dto.HubStaffResponse;
+import org.springframework.cloud.openfeign.CollectionFormat;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ public interface HubClient {
 	@GetMapping("/api/v1/hub-managers/{managerId}")
 	FeignResponse<HubStaffResponse> getHubStaff(@PathVariable("managerId") UUID managerId);
 
+	@CollectionFormat(feign.CollectionFormat.CSV)
 	@GetMapping("/api/v1/hubs")
 	FeignResponse<List<HubResponse>> getHubs(@RequestParam("hubIds") List<UUID> hubIds);
 }
