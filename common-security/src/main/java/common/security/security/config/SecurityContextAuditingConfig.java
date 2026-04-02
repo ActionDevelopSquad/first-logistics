3bc7d9f4405eb77b6d964ac1381e8c7bee313557
Lim JinKeon <0907john24@gmail.com>
2026-04-02 13:08:14 +0900

    fix: 어노테이션 수정

--- common-security/src/main/java/common/security/security/config/SecurityContextAuditingConfig.java
+++ common-security/src/main/java/common/security/security/config/SecurityContextAuditingConfig.java
@@ -6,12 +6,12 @@ import common.security.entity.exception.AuthException;
 import common.security.security.util.SecurityUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Optional;
 import java.util.UUID;
 
-@Component
+@Configuration
 @Slf4j
 @AutoConfigureBefore(JpaAuditingConfig.class)
 public class SecurityContextAuditingConfig implements CurrentAuditorProvider {
