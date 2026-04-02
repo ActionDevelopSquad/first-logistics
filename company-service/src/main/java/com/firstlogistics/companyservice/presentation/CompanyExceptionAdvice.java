151a7afa27197075ace81ee6cd45fc98949a58a3
Seungwon-Choi <bestwondev@gmail.com>
2026-04-02 11:32:21 +0900

    feat: DataIntegrityViolationException 핸들링 메서드 추가

--- company-service/src/main/java/com/firstlogistics/companyservice/presentation/CompanyExceptionAdvice.java
+++ company-service/src/main/java/com/firstlogistics/companyservice/presentation/CompanyExceptionAdvice.java
@@ -1,9 +1,23 @@
 package com.firstlogistics.companyservice.presentation;
 
+import com.firstlogistics.companyservice.domain.exception.CompanyErrorCode;
 import common.exception.GlobalExceptionHandler;
+import common.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class CompanyExceptionAdvice extends GlobalExceptionHandler {
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ApiResponse<Void>> handleDataIntegrityViolation(DataIntegrityViolationException e) {
+        log.warn("[DataIntegrityViolationException] message: {}", e.getMessage());
+        return ResponseEntity
+                .status(CompanyErrorCode.DUPLICATE_MANAGER_ID.getStatus())
+                .body(ApiResponse.error(CompanyErrorCode.DUPLICATE_MANAGER_ID));
+    }
 }
