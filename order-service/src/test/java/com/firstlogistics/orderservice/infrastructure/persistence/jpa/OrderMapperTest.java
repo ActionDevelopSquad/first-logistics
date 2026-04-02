8a156e0a202b6a109121a618a7d4b3c48510081e
jaebin1234 <jaebin2586@gmail.com>
2026-04-02 21:40:58 +0900

    fix: 전체 빌드 되게 수정, query-dsl 사용하는 모듈쪽 그래들 수정, order와 al 쪽 test 및 test-yml쪽 수정

--- order-service/src/test/java/com/firstlogistics/orderservice/infrastructure/persistence/jpa/OrderMapperTest.java
+++ order-service/src/test/java/com/firstlogistics/orderservice/infrastructure/persistence/jpa/OrderMapperTest.java
@@ -7,9 +7,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -18,6 +20,8 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import({OrderMapper.class})
 class OrderMapperTest {
 
