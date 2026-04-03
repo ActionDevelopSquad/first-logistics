c2659783a546947113ba5deebfc2e5b8dc629b9f
Sehi55 <sehi1028@naver.com>
2026-04-03 19:17:52 +0900

    fix: jpa repository 상속

--- hub-service/src/main/java/com/firstlogistics/hubservice/hubManager/infrastructure/persistence/jpa/HubManagerJpaRepository.java
+++ hub-service/src/main/java/com/firstlogistics/hubservice/hubManager/infrastructure/persistence/jpa/HubManagerJpaRepository.java
@@ -1,4 +1,8 @@
 package com.firstlogistics.hubservice.hubManager.infrastructure.persistence.jpa;
 
-public interface HubManagerJpaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface HubManagerJpaRepository extends JpaRepository<HubManagerJpaEntity, UUID> {
 }
