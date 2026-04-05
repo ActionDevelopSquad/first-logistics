7353098b65f00f1a41a2548ef477fbd4fb3aafc3
Sehi55 <sehi1028@naver.com>
2026-04-06 03:06:26 +0900

    feat: 캐시 설정 추가

---
+++ hub-service/src/main/java/com/firstlogistics/hubservice/hubManager/infrastructure/cache/config/CacheConfig.java
@@ -0,0 +1,52 @@
+package com.firstlogistics.hubservice.hubManager.infrastructure.cache.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        GenericJackson2JsonRedisSerializer serializer =
+                new GenericJackson2JsonRedisSerializer(objectMapper);
+
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(serializer)
+                )
+                .entryTtl(Duration.ofDays(1))
+                .disableCachingNullValues();
+
+        Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+        cacheConfigurations.put("hub:byId", defaultConfig.entryTtl(Duration.ofDays(30)));
+        cacheConfigurations.put("hub:all", defaultConfig.entryTtl(Duration.ofDays(30)));
+        cacheConfigurations.put("hubConnection:all", defaultConfig.entryTtl(Duration.ofDays(30)));
+        cacheConfigurations.put("route:hybrid:result", defaultConfig.entryTtl(Duration.ofDays(10)));
+        cacheConfigurations.put("route:hubTohub:result", defaultConfig.entryTtl(Duration.ofDays(10)));
+
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(defaultConfig)
+                .withInitialCacheConfigurations(cacheConfigurations)
+                .build();
+    }
+}
