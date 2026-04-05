9bdb02f07d7bfc511e9f9bff9b86c406f9b7913c
haeun228 <123haeun@naver.com>
2026-04-05 15:56:10 +0900

    refactor: OrderQueryService 읽기 전용 트랜잭션 적용

--- order-service/src/main/java/com/firstlogistics/orderservice/application/OrderQueryService.java
+++ order-service/src/main/java/com/firstlogistics/orderservice/application/OrderQueryService.java
@@ -14,11 +14,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class OrderQueryService {
 
     private final OrderQueryRepository orderQueryRepository;
