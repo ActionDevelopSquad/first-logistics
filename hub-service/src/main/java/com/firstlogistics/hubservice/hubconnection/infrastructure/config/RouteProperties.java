123914eace411ed6e1a2f3651230bdf5044ba4d6
Sehi55 <sehi1028@naver.com>
2026-04-06 01:11:25 +0900

    fix : defaultPolicy를 필수값으로 강제

--- hub-service/src/main/java/com/firstlogistics/hubservice/hubconnection/infrastructure/config/RouteProperties.java
+++ hub-service/src/main/java/com/firstlogistics/hubservice/hubconnection/infrastructure/config/RouteProperties.java
@@ -1,15 +1,19 @@
 package com.firstlogistics.hubservice.hubconnection.infrastructure.config;
 
 import com.firstlogistics.hubservice.hubconnection.domain.enums.RoutePolicy;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
+@Validated
 @ConfigurationProperties(prefix = "route")
 @Component
 public class RouteProperties {
+    @NotNull
     private RoutePolicy defaultPolicy;
 }
