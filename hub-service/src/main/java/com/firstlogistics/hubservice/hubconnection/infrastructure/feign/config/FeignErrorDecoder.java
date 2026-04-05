8ff2fdc2acc3a6aba5337b7a8d8b9fa4c701b7da
Sehi55 <sehi1028@naver.com>
2026-04-06 01:31:00 +0900

    refactor: 에러 처리 로그 추가, 문자열 상수화

--- hub-service/src/main/java/com/firstlogistics/hubservice/hubconnection/infrastructure/feign/config/FeignErrorDecoder.java
+++ hub-service/src/main/java/com/firstlogistics/hubservice/hubconnection/infrastructure/feign/config/FeignErrorDecoder.java
@@ -5,16 +5,22 @@ import com.firstlogistics.hubservice.hubconnection.domain.exception.HubConnectio
 import com.firstlogistics.hubservice.hubconnection.domain.exception.HubConnectionException;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class FeignErrorDecoder implements ErrorDecoder {
+    private static final String COMPANY_CLIENT_PREFIX = "CompanyClient#";
+    private static final String NAVER_MAP_CLIENT_PREFIX = "NaverMapClient#";
 
     @Override
     public Exception decode(String methodKey, Response response) {
+        log.warn("Feign call failed. methodKey={}, status={}", methodKey, response.status());
+
         if (response.status() == 404) {
-            if (methodKey.startsWith("CompanyClient#")) {
+            if (methodKey.startsWith(COMPANY_CLIENT_PREFIX)) {
                 return new HubConnectionException(HubConnectionErrorCode.COMPANY_NOT_FOUND);
             }
-            if (methodKey.startsWith("NaverMapClient#")) {
+            if (methodKey.startsWith(NAVER_MAP_CLIENT_PREFIX)) {
                 return new HubConnectionException(HubConnectionErrorCode.ROUTE_METRIC_UNAVAILABLE);
             }
         }
