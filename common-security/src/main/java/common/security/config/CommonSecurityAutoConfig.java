7d73de2d5fd3b0757a6dca4b3834c00b1961ec35
jaebin1234 <jaebin2586@gmail.com>
2026-04-07 02:39:50 +0900

    fix: common-security 오토 컨피그레이션 로드와 빈 생성 문제

--- common-security/src/main/java/common/security/config/CommonSecurityAutoConfig.java
+++ common-security/src/main/java/common/security/config/CommonSecurityAutoConfig.java
@@ -3,8 +3,10 @@ package common.security.config;
 import common.security.aop.RoleCheckAspect;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 @AutoConfiguration
+@Import({SecurityConfig.class, FeignAuthPropagationConfig.class})
 public class CommonSecurityAutoConfig {
 
     @Bean
--- common-security/src/main/java/common/security/config/FeignAuthPropagationConfig.java
+++ common-security/src/main/java/common/security/config/FeignAuthPropagationConfig.java
@@ -6,13 +6,13 @@ import common.security.util.SecurityUtils;
 import feign.RequestInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-@AutoConfiguration
+@Configuration
 @Slf4j
 public class FeignAuthPropagationConfig {
 
