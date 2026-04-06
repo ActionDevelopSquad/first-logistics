bf7d1b1b344f53fa65a82e92fec64cecba2d932e
Sehi55 <sehi1028@naver.com>
2026-04-06 14:08:03 +0900

    refactor: 사용되지 않는 Import 삭제

--- hub-service/src/main/java/com/firstlogistics/hubservice/hubconnection/infrastructure/persistence/jpa/HubConnectionRepositoryImpl.java
+++ hub-service/src/main/java/com/firstlogistics/hubservice/hubconnection/infrastructure/persistence/jpa/HubConnectionRepositoryImpl.java
@@ -9,7 +9,6 @@ import com.firstlogistics.hubservice.hubconnection.domain.vo.HubConnectionId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.Caching;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Repository;
 
