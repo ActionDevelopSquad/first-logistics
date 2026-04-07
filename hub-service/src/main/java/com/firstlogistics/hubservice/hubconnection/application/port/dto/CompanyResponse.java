54ad9b7fb2edc88f2a75ea71faac82476fad11e6
jaebin1234 <jaebin2586@gmail.com>
2026-04-07 18:24:44 +0900

    fix:허브경로에서 마지막 회사 가져오는게 매핑 변수명 오류

--- hub-service/src/main/java/com/firstlogistics/hubservice/hubconnection/application/port/dto/CompanyResponse.java
+++ hub-service/src/main/java/com/firstlogistics/hubservice/hubconnection/application/port/dto/CompanyResponse.java
@@ -1,12 +1,13 @@
 package com.firstlogistics.hubservice.hubconnection.application.port.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.UUID;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record CompanyResponse(
-        UUID companyId,
+        @JsonProperty("id") UUID companyId,
         UUID hubId,
         double latitude,
         double longitude
